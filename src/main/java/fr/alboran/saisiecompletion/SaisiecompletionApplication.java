c152051a50e185c4b6ed8470bc5dfc99c659f1e3
Alboran <97444137+ImAlboran@users.noreply.github.com>
2023-07-29 23:15:25 +0200

    Initial commit

---
+++ src/main/java/fr/alboran/saisiecompletion/SaisiecompletionApplication.java
@@ -0,0 +1,13 @@
+package fr.alboran.saisiecompletion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaisiecompletionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaisiecompletionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/alboran/saisiecompletion/SaisiecompletionApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.alboran.saisiecompletion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaisiecompletionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
